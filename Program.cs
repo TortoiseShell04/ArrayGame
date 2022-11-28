a4b1715f968a4364f94b745ff4ae8a494bc161a5
TortoiseShell04 <cutiecatdaog@gmail.com>
2022-11-28 16:45:25 +0200

    Changes to be committed: 	modified:   Program.cs

--- Program.cs
+++ Program.cs
@@ -6,27 +6,15 @@ namespace App
         static void Main(string[] args)
         { 
             // MISC DECLARATION
-            int moveCount = 0;
             bool gameIsOn = true;
             bool gameHasStarted = false;
+            bool gameLevel = true;
             char cntrl = 'i';
-
-            // PLAYER CORDS
-            Random ran = new Random();
-            int xP = ran.Next((int) 9);
-            if (xP == 0) {xP++;}
-            int yP = ran.Next((int) 9);
-            if (yP == 0) {yP++;}
-
-            // BOX CORDS
-            Random box = new Random();
-            int xB = box.Next((int) 9);
-            if (xB == 0) {xB++;}
-            int yB = box.Next((int) 9);
-            if (yB == 0) {yB++;}
+            int[] boxDim = new int[] {0,0};
+            int[] oldDim = new int[] {0,0};
 
             // GAME ZONE
-            int[] oldDim = new int[] {xP,yP};
+            
             int[,] game = new int[,] {  {8,8,8,8,8,8,8,8,8,8}
                                        ,{8,0,0,0,0,0,0,0,0,8}
                                        ,{8,0,0,0,0,0,0,0,0,8}
@@ -38,71 +26,109 @@ namespace App
                                        ,{8,0,0,0,0,0,0,0,0,8} 
                                        ,{8,8,8,8,8,8,8,8,8,8} };
 
-
-            // GAME START
-            for (game[oldDim[0],oldDim[1]] = 1; gameIsOn ; moveCount++)
+                
+            while (gameLevel)
             {
-                if (gameHasStarted == false)
+                gameIsOn = true;
+                if (gameHasStarted == true)
                 {
-                    int[] boxDim = new int[] {xB,yB};
-                    game[boxDim[0],boxDim[1]] = 3;
-                    gameHasStarted = true;
+                   game[oldDim[0],oldDim[1]] = 0;
+                   gameHasStarted = false;
                 }
 
+                // GAME START
+                for (int moveCount = 0; gameIsOn ; moveCount++)
+                {
+                 if (gameHasStarted == false)
+                 {
+                    // PLAYER CORDS
+                    Random ran = new Random();
+                    int xP = ran.Next((int) 9);
+                    if (xP == 0) {xP++;}
+                    int yP = ran.Next((int) 9);
+                    if (yP == 0) {yP++;}
+                    oldDim[0] = xP; oldDim[1] = yP;
 
-             for (int x = 0; x < 10; x++)
-              {
-                    for (int y = 0; y < 10; y++)
-                   {
-                       if (y == 9)
-                         {
-                            Console.WriteLine(game[x,y]);
-                         }
-                        else 
+                    // BOX CORDS
+                    Random box = new Random();
+                    int xB = box.Next((int) 9);
+                    if (xB == 0) {xB++;}
+                    int yB = box.Next((int) 9);
+                    if (yB == 0) {yB++;}
+                    
+
+                    boxDim[0] = xB; boxDim[1] = yB;
+                    game[boxDim[0],boxDim[1]] = 3;
+
+                    game[oldDim[0],oldDim[1]] = 1;
+                    gameHasStarted = true;
+
+                    }
+
+
+                 for (int x = 0; x < 10; x++)
+                     {
+                       for (int y = 0; y < 10; y++)
+                      {
+                         if (y == 9)
                           {
-                            Console.Write(game[x,y]);
+                            Console.WriteLine(game[x,y]);
                           }
+                         else 
+                              {
+                            Console.Write(game[x,y]);
+                               }
+                      }
+
+
                      }
 
+                    if (oldDim[0] == boxDim[0] && oldDim[1] == boxDim[1])
+                        {
+                           Console.WriteLine("You WON the game!!");
+                           gameIsOn = false;
+                           break;
+                    
+                        }
 
-             }
 
-             // PLAYER CONTROL
-             Console.WriteLine("Enter yout move: (W,A,S,D)");
-                cntrl = Convert.ToChar(Console.ReadLine());
+                  // PLAYER CONTROL
+                  Console.WriteLine("Enter yout move: (W,A,S,D)");
+                   cntrl = Convert.ToChar(Console.ReadLine());
 
-                if (cntrl == 'w' || cntrl == 'W')
-                {
+                  if (cntrl == 'w' || cntrl == 'W')
+                  {
                     game[oldDim[0],oldDim[1]] = 0;
                     oldDim[0] -= 1;
                     if (oldDim[0] == 0) {Console.WriteLine("Invalid Move!!"); oldDim[0] += 1;}
                     game[oldDim[0],oldDim[1]] = 1;
-                }
-                if (cntrl == 'a' || cntrl == 'A')
-                {
+                  }
+                  if (cntrl == 'a' || cntrl == 'A')
+                  {
                     game[oldDim[0],oldDim[1]] = 0;
                     oldDim[1] -= 1;
                     if (oldDim[1] == 0) {Console.WriteLine("Invalid Move!!"); oldDim[1] += 1;}
                     game[oldDim[0],oldDim[1]] = 1;
-                }
-                if (cntrl == 's' || cntrl == 'S')
-                {
+                  }
+                  if (cntrl == 's' || cntrl == 'S')
+                  {
                     game[oldDim[0],oldDim[1]] = 0;
                     oldDim[0] += 1;
                     if (oldDim[0] == 9) {Console.WriteLine("Invalid Move!!"); oldDim[0] -= 1;}
                     game[oldDim[0],oldDim[1]] = 1;
-                }
-                if (cntrl == 'd' || cntrl == 'D')
-                {
+                   }
+                   if (cntrl == 'd' || cntrl == 'D')
+                  {
                     game[oldDim[0],oldDim[1]] = 0;
                     oldDim[1] += 1;
                     if (oldDim[1] == 9) {Console.WriteLine("Invalid Move!!"); oldDim[1] -= 1;}
                     game[oldDim[0],oldDim[1]] = 1;
-                }
+                  }
                 
-            }
+             }
                                 
                                
+            }
         }
     }
 }
